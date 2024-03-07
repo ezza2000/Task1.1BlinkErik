118b7ddd817291d9079219f9dfa67aba822ace51
ezza2000 <131439627+ezza2000@users.noreply.github.com>
2024-03-07 11:34:12 +1100

    FIRST ADRUINO PROGRAM

---
+++ blinking_led_erik1.ino
@@ -0,0 +1,44 @@
+int ledPin=8; 
+void setup()
+{
+    pinMode(ledPin,OUTPUT);   
+}
+void loop()
+{  
+  digitalWrite(ledPin, HIGH);  
+  delay(1000);                      
+  digitalWrite(ledPin, LOW);   
+  delay(1000);                   
+  digitalWrite(ledPin, HIGH); 
+  delay(1000);
+  digitalWrite(ledPin, LOW); 
+  delay(500);
+  digitalWrite(ledPin, HIGH); 
+  delay(3000);
+  digitalWrite(ledPin, LOW); 
+  delay(500);
+  digitalWrite(ledPin, HIGH); 
+  delay(1000);
+  digitalWrite(ledPin, LOW); 
+  delay(1000);
+  digitalWrite(ledPin, HIGH); 
+  delay(1000);
+  digitalWrite(ledPin, LOW); 
+  delay(500);
+  digitalWrite(ledPin, HIGH); 
+  delay(1000);
+  digitalWrite(ledPin, LOW); 
+  delay(1000);
+  digitalWrite(ledPin, HIGH); 
+  delay(3000);
+  digitalWrite(ledPin, LOW); 
+  delay(500);
+  digitalWrite(ledPin, HIGH); 
+  delay(1000);
+  digitalWrite(ledPin, LOW); 
+  delay(500);
+  digitalWrite(ledPin, HIGH); 
+  delay(3000);
+  digitalWrite(ledPin, LOW);   
+  delay(10000);    
+}
